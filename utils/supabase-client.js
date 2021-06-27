469c2fe7599ed2c7fdd03dca748cf2abee781560
gonzalo10 <gonzalo.achiaga@gmail.com>
2021-06-27 20:44:25 +0200

    chore: commit

--- utils/supabase-client.js
+++ utils/supabase-client.js
@@ -5,21 +5,29 @@ export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
+async function test() {
+  const { data, error } = await supabase.from('products').select('*');
+
+  console.log(data, error);
+}
+
 export const getActiveProductsWithPrices = async () => {
-  const { data, error } = await supabase
-    .from('products')
-    .select('*, prices(*)')
-    .eq('active', true)
-    .eq('prices.active', true)
-    .order('metadata->index')
-    .order('unit_amount', { foreignTable: 'prices' });
+  await test();
+  // const { data, error } = await supabase
+  //   .from('products')
+  //   .select('*, prices(*)')
+  //   .eq('active', true)
+  //   .eq('prices.active', true)
+  //   .order('metadata->index')
+  //   .order('unit_amount', { foreignTable: 'prices' });
 
-  if (error) {
-    console.log(error.message);
-    throw error;
-  }
+  // if (error) {
+  //   console.log(error.message);
+  //   throw error;
+  // }
 
-  return data || [];
+  // return data || [];
+  return [];
 };
 
 export const updateUserName = async (user, name) => {
